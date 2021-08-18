136ec4a0057a822f28de1015a3e8596f5ff96a99
DavidVH <60264680+dvaqueroh@users.noreply.github.com>
2021-08-18 09:30:57 +0200

    actualizo codigo

--- src/main/java/es/geekshubs/academy/FizzBuzz.java
+++ src/main/java/es/geekshubs/academy/FizzBuzz.java
@@ -2,13 +2,28 @@ package es.geekshubs.academy;
 
 public class FizzBuzz {
 
-  /**
-   * Apply Method
-   * @param count
-   * @return
-   */
+	/*
+	   * Apply Method
+	   * @param count
+	   * @return
+	   */
+	
   public String apply(final int count) {
+	  
+	  for (int i = 1; i < count; i++) {
+		  if(i%3==0) {// si el multiplo de 3
+			  System.out.println("GREEKS"); 
+		  }
+		  else if(i%5==0) {// si el multiplo de 3
+			  System.out.println("HUBS");
+		  }
+		  else { // si no lo es de ninguno de los dos
+			  System.out.println(i);
+		  }
+		  
+		
+	  }// fin for
    return null;
-  }
-
-}
+  }// fin apply
+  
+}// fin class
